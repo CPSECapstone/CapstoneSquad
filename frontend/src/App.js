4080a63a2b1c022cddea1baa1ecbd3f6921ab7ee
AJ Jiro <ajjiro@MacBook-Pro-2.local>
2020-10-26 18:51:35 -0700

    Buttons and scholarship

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import scholarship from './scholarship.jpg';
 import './App.css';
+import styled, { css } from 'styled-components';
 
+
+const Button = styled.button`
+  /* Same as above */
+`;
+const ButtonToggle = styled(Button)`
+  opacity: 0.6;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+  `}
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+const types = ['Redo', 'Undo', 'Annotate'];
+function ToggleGroup() {
+  const [active, setActive] = useState(types[0]);
+  return (
+    <ButtonGroup>
+      {types.map(type => (
+        <ButtonToggle
+          key={type}
+          active={active === type}
+          onClick={() => setActive(type)}
+        >
+          {type}
+        </ButtonToggle>
+      ))}
+    </ButtonGroup>
+  );
+}
 function App() {
+  const [active, setActive] = useState(types[0]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ButtonGroup>
+      {types.map(type => (
+        <ButtonToggle
+          key={type}
+          active={active === type}
+          onClick={() => setActive(type)}
         >
-          Learn React
+          {type}
+        </ButtonToggle>
+      ))}
+    </ButtonGroup>
+        
+        <img src={scholarship} className="App-scholarship" alt="scholarship" />
+        <p>
+        </p>
+        <a>
+          Calm Your Thoughts With Tater Tots
         </a>
       </header>
     </div>
+    
   );
 }
 
