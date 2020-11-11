337886ae337177cb9726da2ae27ea11b32745aee
AJ Jiro <ajjiro@MacBook-Pro-2.local>
2020-11-11 14:53:26 -0800

    Starter Code for Canvas API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import scholarship from './scholarship.jpg';
 import './App.css';
 import styled, { css } from 'styled-components';
@@ -37,6 +37,46 @@ function ToggleGroup() {
 }
 function App() {
   const [active, setActive] = useState(types[0]);
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+  
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d")
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+   contextRef.current.closePath()
+   setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if(!isDrawing){
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -51,15 +91,27 @@ function App() {
         </ButtonToggle>
       ))}
     </ButtonGroup>
-        
+    
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
+{/*
         <img src={scholarship} className="App-scholarship" alt="scholarship" />
         <p>
         </p>
         <a>
           Calm Your Thoughts With Tater Tots
         </a>
+*/
+}
       </header>
     </div>
+
+
+    
     
   );
 }
