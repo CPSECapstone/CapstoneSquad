0644d7859f8773b0175d213330f9a8c54471baac
AJ Jiro <ajjiro@MacBook-Pro-2.local>
2021-01-17 12:41:33 -0800

    Saving and Innotating

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,24 +1,13 @@
+
 import React, { Component } from "react";
 import { render } from "react-dom";
-import scholarship from './scholarship.jpg';
-import text from './text.jpg'
-import styled, { css } from 'styled-components';
-import redo from './redo.jpg'
-import undo from './undo.jpg'
-import draw from './draw.jpg'
-import highlight from './highlight.jpg'
-import erase from './erase.jpg'
-import './App.css';
+import Scholarship from './Scholarship.jpg';
+import Save from './Save.jpg'
+import Undo from './Undo.jpg'
+import Clear from './Clear.jpg'
 import CanvasDraw from "../../src";
 import classNames from "./index.css";
 
-const Button = styled.button``;
-const ButtonToggle = styled(Button)`
-  opacity: 10;
-  ${({ active }) =>
-    active && `opacity: 100;`}
-`;
-const ButtonGroup = styled.div` `;
 class Demo extends Component {
   state = {
     color: "#ffc600",
@@ -27,63 +16,138 @@ class Demo extends Component {
     brushRadius: 10,
     lazyRadius: 12
   };
- 
+
   render() {
     return (
+      <div>
+       
+        <h2>Save & Load</h2>
+        <p>
+          This part got me most excited. Very easy to use saving and loading of
+          drawings. It even comes with a customizable loading speed to control
+          whether your drawing should load instantly (loadTimeOffset = 0) or
+          appear after some time (loadTimeOffset > 0)
+          <span>{`<CanvasDraw loadTimeOffset={10} />`}</span>
+        </p>
+        <p>Try it out! Draw something, hit "Save" and then "Load".</p>
+        <div className={classNames.tools}>
+          <button
+            style={{height: '230px', width : '240px'}}
+            onClick={() => {
+              localStorage.setItem(
+                "savedDrawing",
+                this.saveableCanvas.getSaveData()
+              );
+            }}
+          >
+           
+              <img src={Save}/>
+          </button>
+          <button
+            style={{height: '230px', width : '240px'}}
+            onClick={() => {
+              this.saveableCanvas.clear();
+            }}
 
-      <div className="App">
-      
-     
-      <header > 
-        <ButtonGroup> 
-        <ButtonToggle>
-        <img src={redo} className="App-buttons" />
-        </ButtonToggle>
-        <ButtonToggle>
-        <img src={undo} className="App-buttons"/>
-        </ButtonToggle>
-    </ButtonGroup>
-    <CanvasDraw
-          brushColor="rgba(0,0,0,0.3)"
-          imgSrc={scholarship}
-        /> 
-         <h2>Calm Your Thoughts With Tater Tots</h2>
-    </header>
-
-    
-
-    <header className="App-RightButton"> 
-        <ButtonGroup> 
-        <ButtonToggle>
-        <img src={text} className="App-buttons" />
-        </ButtonToggle>
-    </ButtonGroup>
-    </header>
-
-    <header className="App-SecondRightButton"> 
-        <ButtonGroup> 
-        <ButtonToggle>
-        <img src={draw} className="App-buttons" />
-        </ButtonToggle>
-    </ButtonGroup>
-    </header>
-
-
-    <header className="App-ThirdRightButton"> 
-        <ButtonGroup> 
-        <ButtonToggle>
-        <img src={highlight} className="App-buttons" />
-        </ButtonToggle>
-    </ButtonGroup>
-    </header>
-
-    <header className="App-FourthRightButton"> 
-        <ButtonGroup> 
-        <ButtonToggle>
-        <img src={erase} className="App-buttons" />
-        </ButtonToggle>
-    </ButtonGroup>
-    </header>
+          >
+            <img src={Clear}/>
+          </button>
+          <button
+            style={{height: '230px', width : '240px'}}
+            onClick={() => {
+              this.saveableCanvas.undo();
+            }}
+            Clear
+          >
+             <img src={Undo}/>
+          </button>
+          
+          <div>
+            <label>Width:</label>
+            <input
+              type="number"
+              value={this.state.width}
+              onChange={e =>
+                this.setState({ width: parseInt(e.target.value, 10) })
+              }
+            />
+          </div>
+          <div>
+            <label>Height:</label>
+            <input
+              type="number"
+              value={this.state.height}
+              onChange={e =>
+                this.setState({ height: parseInt(e.target.value, 10) })
+              }
+            />
+          </div>
+          <div>
+            <label>Brush-Radius:</label>
+            <input
+              type="number"
+              value={this.state.brushRadius}
+              onChange={e =>
+                this.setState({ brushRadius: parseInt(e.target.value, 10) })
+              }
+            />
+          </div>
+          <div>
+            <label>Lazy-Radius:</label>
+            <input
+              type="number"
+              value={this.state.lazyRadius}
+              onChange={e =>
+                this.setState({ lazyRadius: parseInt(e.target.value, 10) })
+              }
+            />
+          </div>
+        </div>
+        <CanvasDraw
+          imgSrc={Scholarship}
+          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+          brushColor={this.state.color}
+          brushRadius={this.state.brushRadius}
+          lazyRadius={this.state.lazyRadius}
+          canvasWidth={this.state.width}
+          canvasHeight={this.state.height}
+          
+        />
+        <p>
+          The following is a disabled canvas with a hidden grid that we use to
+          load & show your saved drawing.
+        </p>
+        <button
+          onClick={() => {
+            this.loadableCanvas.loadSaveData(
+              localStorage.getItem("savedDrawing")
+            );
+          }}
+        >
+          Load what you saved previously into the following canvas. Either by
+          calling `loadSaveData()` on the component's reference or passing it
+          the `saveData` prop:
+        </button>
+        <CanvasDraw
+          imgSrc={Scholarship}
+          disabled
+          hideGrid
+          ref={canvasDraw => (this.loadableCanvas = canvasDraw)}
+          saveData={localStorage.getItem("savedDrawing")}
+        />
+        <p>
+          The saving & loading also takes different dimensions into account.
+          Change the width & height, draw something and save it and then load it
+          into the disabled canvas. It will load your previously saved
+          masterpiece scaled to the current canvas dimensions.
+        </p>
+        <p>
+          That's it for now! Take a look at the{" "}
+          <a href="https://github.com/mBeierl/react-canvas-draw/tree/master/demo/src">
+            source code of these examples
+          </a>
+          .
+        </p>
       </div>
     );
   }
