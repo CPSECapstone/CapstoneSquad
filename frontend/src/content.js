d7c59b14e8e87003f0e59265ed4c39c1936d910e
AJ Jiro <ajjiro@MacBook-Pro-2.local>
2021-05-05 18:57:36 -0700

    At least it's on the screen

--- frontend/src/content.js
+++ frontend/src/content.js
@@ -3,25 +3,160 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import "./content.css";
 import Scholarship from './Scholarship.jpg';
-//import Save from './Save.jpg'
-//import Undo from './Undo.jpg'
-//import Clear from './Clear.jpg'
-//import classNames from "./index.css";
+import Save from './Save.jpg'
+import Undo from './Undo.jpg'
+import Clear from './Clear.jpg'
+import classNames from "./index.css";
 import CanvasDraw from "react-canvas-draw";
 
 class Main extends React.Component {
   state = {
-    color: "#RRGGBBAA",
+    color: "#ffc600",
     width: 400,
     height: 400,
     brushRadius: 5,
-    lazyRadius: 5
+    lazyRadius: 5,
   };
     render() {
         return (
+          <div>
+         
+          <h2>Scholarship Annotator</h2>
+
+          <p id="p1">Try it out! Draw something, hit "Save" and then "Load".</p>
+          <div class="buttons" className={classNames.tools}>
             <CanvasDraw
-           
+              ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+              brushColor={this.state.color}
+              brushRadius={this.state.brushRadius}
+              lazyRadius={this.state.lazyRadius}
+              canvasWidth={this.state.width}
+              canvasHeight={this.state.height}
+              hideGrid
+            
+             
+              
             />
+            <button
+              style={{height: '50px', width : '50px'}}
+              onClick={() => {
+                localStorage.setItem(
+                  "savedDrawing",
+                  this.saveableCanvas.getSaveData()
+                );
+              }}
+            >
+              <img src={Save} height='40' alt ="Save Button"/>
+
+            </button>
+
+            <button
+              style={{height: '50px', width : '50px'}}
+              onClick={() => {
+                this.saveableCanvas.clear();
+              }}
+  
+            >
+              <img src={Clear} height='40' alt = "Clear Button"/>
+            </button>
+
+            <button
+              style={{height: '50px', width : '50px'}}
+              onClick={() => {
+                this.saveableCanvas.undo();
+              }}
+              Clear
+            >
+               <img src={Undo} height='40' width='40' alt = "Undo Button"/>
+            </button>
+
+             {/*
+            <div>
+              <label>Width:</label>
+              <input
+                type="number"
+                value={this.state.width}
+                onChange={e =>
+                  this.setState({ width: parseInt(e.target.value, 10) })
+                }
+              />
+            </div>
+            */}
+            {/*
+            <div>
+              <label>Height:</label>
+              <input
+                type="number"
+                value={this.state.height}
+                onChange={e =>
+                  this.setState({ height: parseInt(e.target.value, 10) })
+                }
+              />
+            </div>
+            */}
+            
+            
+            <div>
+              <label>Brush-Radius:</label>
+              <input
+                type="number"
+                value={this.state.brushRadius}
+                onChange={e =>
+                  this.setState({ brushRadius: parseInt(e.target.value, 10) })
+                }
+              />
+            </div>
+            <div>
+              <label>Drag-Radius:</label>
+              <input
+                type="number"
+                value={this.state.lazyRadius}
+                onChange={e =>
+                  this.setState({ lazyRadius: parseInt(e.target.value, 10) })
+                }
+              />
+            </div>
+
+            <form>
+
+               <label for="colorpicker">Color Picker:</label>
+               <input
+                  id="brushColorPicker"
+                  type="color"
+                  onChange={e =>
+                    this.setState({ color: e.target.value })
+                    }
+               />
+
+            </form>
+
+          </div>
+          
+          <p>
+            See your saved template below!
+          </p>
+          {/*<p>{this.state.backendResponse}</p>*/}
+          <div class="save" className={classNames.tools}>
+            <button
+              style= {{order: 2, height: '100px', width: '300px'}}
+              onClick={() => {
+                this.loadableCanvas.loadSaveData(
+                  localStorage.getItem("savedDrawing")
+                );
+              }}
+            >
+              CLICK ME! To see your saved changes.
+            </button>
+            <CanvasDraw
+              imgSrc={Scholarship}
+              disabled
+              hideGrid
+              ref={canvasDraw => (this.loadableCanvas = canvasDraw)}
+              saveData={localStorage.getItem("savedDrawing")}
+            />
+          </div>
+        </div>
+            
         )
     }
 }
